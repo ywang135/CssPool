62b29a26f6541e94dff861485f6e6b2bd98b3bd4
Yi Wang <yige@Yi.local>
2014-08-13 13:31:26 -0700

    index page beta-1

---
+++ yisite/csspool/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+
+from csspool import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index')
+#    url(r'redirect$', views.redirect_index, name='redirect_index'),
+#    url(r'signup$', views.signup, name='signup'),
+#    url(r'signin$', views.signin, name='signin'),
+#    url(r'showcss$', views.showcss, name='showcss'),
+#    url(r'addcss$', views.addcss, name='addcss'),
+#    url(r'logout$', views.logout, name='logout'),
+#    url(r'success$', views.success, name='success'),
+#    url(r'ajaxaccesscss/(\d{1,7})$', views.ajaxaccesscss, name='ajaxaccesscss')
+)
--- yisite/csspool/views.py
+++ yisite/csspool/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from models import User, CssPool
+from django.db import IntegrityError, transaction
 
-# Create your views here.
+import datetime
+import time
+import hashlib
+import json
+
+def index(request):
+    template = loader.get_template('csspool/index.html')
+    curtime = time.strftime("%x, %a")
+    context = RequestContext(request, {
+        'current_date': curtime,
+    })
+    return HttpResponse(template.render(context))
